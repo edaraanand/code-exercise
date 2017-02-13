b3cffa1739f12730ad1454d9ba9ace8d3bbfb691
edar2252 <edara.babu@rackspace.com>
2017-02-13 17:30:20 -0600

    Refactor Party Model

--- app/models/party.rb
+++ app/models/party.rb
@@ -15,19 +15,20 @@
 #
 class Party < ApplicationRecord
 
+  # Make sure these parameters exist or it throws an error in validations for 'length' method
+  validates :host_name, :host_email, :venue, :location, :theme, :when, :guest_names, presence: true
+
   validate :validations
 
   def validations
-    if host_name.length<255 || host_email.length<255 || venue.length<255 || location.size<255 || theme.size<255
+    if host_name.length>255 || host_email.length>255 || venue.length>255 || location.size>255 || theme.size>255
       errors.add(:base,"Input was too long.")
     end
     # ruby doesn't like us using when as column name for some reason
     if self[:when]>when_its_over
       errors.add(:base,"Incorrect party time.")
     end
-    if numgsts.nil?
-      numgsts = 0
-    end
+    numgsts = 0 if numgsts.nil?
     if venue.length > 0 && location.length < 0
       errors.add(:location,"Where is the party?")
     end
@@ -36,16 +37,21 @@ class Party < ApplicationRecord
     end
   end
 
+  # Class Method
+  def self.order_query(sort_key, sort_order)
+    return Party.order("#{sort_key}: #{sort_order}").all
+  end
+
   def after_save
     # clean "Harry S. Truman" guest name to "Harry S._Truman"
     # clean "Roger      Rabbit" guest name to "Roger Rabbit"
     gnames = []
-    guest_names.split(',').each do |g|
+    self.guest_names.split(',').each do |g|
       g.squeeze!
       names=g.split(' ')
       gnames << "#{names[0]} #{names[1..-1].join('_')}"
     end
-    guest_names = gnames
-    save!
+    self.guest_names = gnames
+    self.save!
   end
 end
